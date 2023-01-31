80bd6e9d4a3b65304707ee54bff68bd7a57e49fc
Sam <bostonswongs@gmail.com>
2023-01-31 01:14:10 -0500

    Integrated the firebase database and updated scoring

--- anagram/app/src/main/java/com/example/anagram/Anagram.java
+++ anagram/app/src/main/java/com/example/anagram/Anagram.java
@@ -1,7 +1,13 @@
 package com.example.anagram;
 
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.app.DownloadManager;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.util.Log;
 
@@ -15,128 +21,140 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.opencsv.CSVReader;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.FileReader;
 import com.google.firebase.FirebaseApp;
 
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Anagram {
-    private static boolean newGame = true;
     public static final Random RANDOM = new Random();
-    public static String[][] WORDS;
-    public static int tier = 1;
+    public static String[][] WORDS = new String[3][2000];
+    public static int tier = 0;
 
-    private static void loadAllWords() {
+    private static int[] WORDS_LEN = {1, 1, 1};
+
+    private static void parseUriCsv(int level, Context context, String file, Uri uri) {
+        final DownloadManager[] manager = new DownloadManager[1];
+        File f = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + file);
+        if(!f.exists()) {
+            manager[0] = (DownloadManager) context.getSystemService(context.DOWNLOAD_SERVICE);
+            DownloadManager.Request request = new DownloadManager.Request(uri);
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,file);
+            long reference = manager[0].enqueue(request);
+        }
+        try {
+            if (!f.exists()) {
+                Log.e("Parse", "File doesn't exist");
+            }
+            FileInputStream fis = new FileInputStream(f);
+            DataInputStream in = new DataInputStream(fis);
+            BufferedReader br =
+                    new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            int i = 0;
+            while ((strLine = br.readLine()) != null) {
+                if(!strLine.isEmpty()) {
+                    WORDS[level][i] = strLine.toUpperCase();
+                    i++;
+                }
+            }
+            Log.e("Parse", level + " File done");
+            WORDS_LEN[level] = i + 1;
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void loadAllWords(Context context) {
         FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference easyRef = storage.getReferenceFromUrl("https://firebasestorage.googleapis.com/v0/b/anagram-208e5.appspot.com/o/Easy.csv");
         StorageReference medRef = storage.getReferenceFromUrl("https://firebasestorage.googleapis.com/v0/b/anagram-208e5.appspot.com/o/Medium.csv");
         StorageReference hardRef = storage.getReferenceFromUrl("https://firebasestorage.googleapis.com/v0/b/anagram-208e5.appspot.com/o/Hard.csv");
 
-        final long ONE_MEGABYTE = 1024 * 1024;
-        tier = 7;
-
-        File rootPath = new File(Environment.getExternalStorageDirectory(), "Downloads");
-        if(!rootPath.exists()) {
-            rootPath.mkdirs();
-        }
-
-        final File localFile = new File(rootPath,"Easy.csv");
-
-        easyRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                Log.e("firebase ",";local tem file created  created " +localFile.toString());
-                //  updateDb(timestamp,localFile.toString(),position);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                Log.e("firebase ",";local tem file not created  created " +exception.toString());
-            }
-        });
-
-
+        WORDS[0][0] = "HELLO";
+        Log.e("firebase ","Trying the downloads");
         easyRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
             public void onSuccess(Uri uri) {
-                // Data for "images/island.jpg" is returns, use this as needed
-                System.out.print(uri);
-                tier = 55;
+                Log.e("firebase ","uri created ?" +uri.toString());
+                parseUriCsv(0, context, "/anagram/easy.csv", uri);
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(Exception exception) {
-                System.out.print("FUCK easy");
-                tier = 3;
+                Log.e("firebase ",";local tem file not created " +exception.toString());
+                WORDS[0][0] = "FAIL";
             }
         });
-        medRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+        medRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
-            public void onSuccess(byte[] bytes) {
-                // Data for "images/island.jpg" is returns, use this as needed
-                System.out.print(bytes);
-                tier = 55;
+            public void onSuccess(Uri uri) {
+                Log.e("firebase ","uri created ?" +uri.toString());
+                parseUriCsv(1, context, "/anagram/medium.csv", uri);
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
-            public void onFailure(@NonNull Exception exception) {
-                System.out.print("FUCK med");
-                tier = 3;
+            public void onFailure(Exception exception) {
+                Log.e("firebase ",";local tem file not created " +exception.toString());
+                WORDS[1][0] = "FAIL";
             }
         });
-        hardRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+        hardRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
-            public void onSuccess(byte[] bytes) {
-                // Data for "images/island.jpg" is returns, use this as needed
-                System.out.print(bytes);
+            public void onSuccess(Uri uri) {
+                Log.e("firebase ","uri created ?" +uri.toString());
+                parseUriCsv(2, context, "/anagram/hard.csv", uri);
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
-            public void onFailure(@NonNull Exception exception) {
-                System.out.print("FUCK hard");
+            public void onFailure(Exception exception) {
+                Log.e("firebase ",";local tem file not created " +exception.toString());
+                WORDS[2][0] = "FAIL";
             }
         });
     }
 
     public static String randomWord(int score) {
         if(score % 10 == 0) {
+            if(tier != 3) {
+                tier++;
+            }
             if(score == 0) {
                 tier = 0;
-                if(newGame) {
-                    loadAllWords();
-                    newGame = false;
-                }
             }
-//            tier++;
         }
-//        if(tier == 55) {
-//            return String.valueOf(hardRef);
-//        }
-        if(tier == 3) {
-            return "HARDY";
+        if(tier == 3 && score > 35) {
+            int randIndex = RANDOM.nextInt(3);
+            return WORDS[randIndex][RANDOM.nextInt(WORDS_LEN[randIndex])];
         }
-        if(tier == 7) {
-            return "YAYAY";
-        }
-        return "UDDERS";
+        return WORDS[tier][RANDOM.nextInt(WORDS_LEN[tier])];
     }
 
     public static String shuffleWord(String word) {
-//        if (word != null  &&  !"".equals(word)) {
-//            char a[] = word.toCharArray();
-//
-//            for (int i = 0; i < a.length; i++) {
-//                int j = RANDOM.nextInt(a.length);
-//                char tmp = a[i];
-//                a[i] = a[j];
-//                a[j] = tmp;
-//            }
-//
-//            return new String(a);
-//        }
+        if (word != null  &&  !"".equals(word)) {
+            char a[] = word.toCharArray();
+
+            for (int i = 0; i < a.length; i++) {
+                int j = RANDOM.nextInt(a.length);
+                char tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+
+            return new String(a);
+        }
 
         return word;
     }
--- anagram/app/src/main/java/com/example/anagram/MainActivity.java
+++ anagram/app/src/main/java/com/example/anagram/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.anagram;
 
+import static com.example.anagram.Anagram.loadAllWords;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,32 +15,69 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.firebase.FirebaseApp;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private TextView wordTv;
+    private TextView wordTv, scoreBoard, highScoreBoard, strikesBoard;
     private EditText wordEnteredTv;
     private Button validate, newGame;
     private String wordToFind;
 
+    Context context;
+
+    private static int score = 0, highScore = 0, strikes = 3, permission = 0;
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public int verifyStoragePermissions(Activity activity) {
+        Toast.makeText(this, "Please Allow Permission for the app to work!", Toast.LENGTH_LONG).show();
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+        return permission;
+    }
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Context context = getApplicationContext();
+        permission = verifyStoragePermissions(this);
+        context = getApplicationContext();
         FirebaseApp.initializeApp(context);
+        loadAllWords(context);
         setContentView(R.layout.activity_main);
         wordTv = (TextView) findViewById(R.id.wordTv);
-//        score = (TextView) findViewById(R.id.highScore);
-//        strikes =
+        highScoreBoard = (TextView) findViewById(R.id.highScoreTV);
+        scoreBoard = (TextView) findViewById(R.id.scoreTV);
+        strikesBoard = (TextView) findViewById(R.id.guessTV);
         wordEnteredTv = (EditText) findViewById(R.id.wordEnteredEt);
         validate = (Button) findViewById(R.id.validate);
         validate.setOnClickListener(this);
         newGame = (Button) findViewById(R.id.newGame);
         newGame.setOnClickListener(this);
-
         newGame();
     }
 
@@ -47,33 +91,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void validate() {
+        if (wordEnteredTv == null) {
+            Toast.makeText(this, "Enter Text please!", Toast.LENGTH_SHORT).show();
+        }
         String w = wordEnteredTv.getText().toString();
-
         if (wordToFind.equals(w)) {
             Toast.makeText(this, "Congratulations ! You found the word " + wordToFind, Toast.LENGTH_SHORT).show();
-//            score++;
-            newWord(0);
+            score++;
+            scoreBoard.setText("Score: " + score);
+            newWord();
         } else {
-            Toast.makeText(this, "Retry !", Toast.LENGTH_SHORT).show();
-//            strikes++;
-//            if(strikes > 3) {
-//                Toast.makeText(this, "3 strikes and you're out!", Toast.LENGTH_SHORT).show();
-//            }
+            strikes--;
+            strikesBoard.setText("Guesses remaining: " + strikes);
+            Toast.makeText(this, "Retry ! " + strikes + " Guesses Remaining.", Toast.LENGTH_SHORT).show();
+            if(strikes == 0) {
+                Toast.makeText(this, "3 strikes and you're out!", Toast.LENGTH_SHORT).show();
+                newGame();
+            }
         }
     }
 
-    private void newWord(int score) {
+    private void newWord() {
         wordToFind = Anagram.randomWord(score);
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            loadAllWords(context);
+        } else {
+            permission = verifyStoragePermissions(this);
+        }
         String wordShuffled = Anagram.shuffleWord(wordToFind);
         wordTv.setText(wordShuffled);
         wordEnteredTv.setText("");
     }
 
     private void newGame() {
-        newWord(0);
-//        wordToFind = Anagram.randomWord(score);
-//        String wordShuffled = Anagram.shuffleWord(wordToFind);
-//        wordTv.setText(wordShuffled);
-//        wordEnteredTv.setText("");
+        if(score > highScore) {
+            highScore = score;
+            Toast.makeText(this, "WOW! A new High Score! " + highScore, Toast.LENGTH_SHORT).show();
+        }
+        score = 0;
+        strikes = 3;
+        strikesBoard.setText("Guesses remaining: " + strikes);
+        highScoreBoard.setText("High Score: " + highScore);
+        scoreBoard.setText("Score: " + score);
+        Toast.makeText(this, "NEW GAME", Toast.LENGTH_SHORT).show();
+        newWord();
     }
 }
